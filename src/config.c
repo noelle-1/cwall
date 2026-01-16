64a8e78b48911ff8dca2fc42ea981b600f653cdc
noelle <contact.me@mailraven.net>
2026-01-16 02:32:11 -0700

    Add more helpful comments to the config.c file

--- src/config.c
+++ src/config.c
@@ -1,7 +1,21 @@
 #include "config.h"
+//the following file contains a premade example that i use on my system. tweak it to fit yours.
 
-char *WALLPAPERS_PATH = "/home/noelle/Pictures/wallpapers/"; //make sure to add a trailing slash to the end
-char *WALLPAPER_CMD = "swaybg -m fill -i "; //if the command requires a space add it
-char *FAVORITES_NAME = "Favorites/"; //Name of the folder to store favorites in 
+char *WALLPAPERS_PATH = "/home/noelle/Pictures/wallpapers/"; //the root wallpapers folder.
+                                                             //MAKE SURE TO ADD A TRAILING SLASH.
+                                                             //ONLY USE FULL PATHS. NO ENV VARS.
+
+char *WALLPAPER_CMD = "swaybg -m fill -i "; //leave a space after the command. the image will be appended to the end.
+                                            //another exampe "feh --bg-fill " (note the space at the end)
+                                            //any wallpaper applying command will work here since its a wrapper.
+
+char *FAVORITES_NAME = "Favorites/"; //Name of the folder to store favorites in
+                                     //The application will create a Folder of this name in the root wallpaper dir
+                                     //MAKE SURE YOU HAVE RW ACCESS IT WILL BREAK IF YOU DONT
+
 uint8_t KILL_COMMAND_FLAG = 0; //set to 1 if you DONT want to execute a kill command.
+                               
 char *KILL_COMMAND = "kill swaybg"; //the KILL_COMMAND, only executed if KILL_COMMAND_FLAG is equal to 0.
+                                    //the kill command is useful for swaybg 
+                                    //because running the command over and over creates lots of processes. 
+                                    //some wallpaper commands, like feh, dont need the kill command.
