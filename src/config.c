891aa178a2dda91f11d50b87edb08b0596c0702b
noelle <contact.me@mailraven.net>
2026-01-16 01:18:58 -0700

    Just change the default config to be useful to me

--- src/config.c
+++ src/config.c
@@ -4,4 +4,4 @@ char *WALLPAPERS_PATH = "/home/noelle/Pictures/wallpapers/"; //make sure to add 
 char *WALLPAPER_CMD = "swaybg -m fill -i "; //if the command requires a space add it
 char *FAVORITES_NAME = "Favorites/"; //Name of the folder to store favorites in 
 uint8_t KILL_COMMAND_FLAG = 0; //set to 1 if you DONT want to execute a kill command.
-char *KILL_COMMAND = "killall swaybg"; //the KILL_COMMAND, only executed if KILL_COMMAND_FLAG is equal to 0.
+char *KILL_COMMAND = "kill swaybg"; //the KILL_COMMAND, only executed if KILL_COMMAND_FLAG is equal to 0.
